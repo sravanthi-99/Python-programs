8afc9d1a71670834721fc1ccddb14d805b6f8ce0
sravanthi-99 <parisesravanthi0@gmail.com>
2025-02-28 16:17:34 +0530

    Create typeconversion.py

---
+++ typeconversion.py
@@ -0,0 +1,15 @@
+#String to integer
+a="4"
+a=int(a)
+print(type(a))
+print(a)
+#Integer to string
+a=input()
+print(type(a))
+a=int(a)
+print(str(a))
+#String to inteeger
+a="Four"
+a=int(a)
+print(type(a))
+print(a)
