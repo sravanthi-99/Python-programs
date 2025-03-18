03a0f445d48459889ed4da2e810ccf20a900c5f0
sravanthi-99 <parisesravanthi0@gmail.com>
2025-03-18 07:41:29 +0530

    Create percentage.py

---
+++ percentage.py
@@ -0,0 +1,10 @@
+#percentage
+percent=int(input())
+if percent>=90:
+   print("First class")
+elif percent<90 and percent>=70:
+   print("Second class")
+elif percent<70 and percent>=60:
+   print("Third class")
+else:
+   print("Pass")
