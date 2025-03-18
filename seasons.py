f86ea59b3b034c52c877bd5b6c3f2bebd8b1d852
sravanthi-99 <parisesravanthi0@gmail.com>
2025-03-18 07:47:49 +0530

    Create seasons.py

---
+++ seasons.py
@@ -0,0 +1,10 @@
+#Season classification
+month=int(input())
+if month==3 or month==4 or month==5:
+   print("Summer")
+elif month==6 or month==7 or month==8 or month==9:
+   print("Rainy")
+elif month==10 or month==11:
+   print("Winter")
+elif month==12 or month==1 or month==2:
+   print("Autumn")
