210843dd3280dfd4b960e6cef9a6a13ada884f61
sravanthi-99 <parisesravanthi0@gmail.com>
2025-03-18 07:54:09 +0530

    Create palindeome.py

---
+++ palindeome.py
@@ -0,0 +1,23 @@
+#palindromes check
+a="MADAM"
+b=a[::-1]
+if a==b:
+  print("Palindrome")
+else:
+  print("not palindrome")
+
+
+a="MADAM SOS"
+b=a[::-1]
+if a==b:
+  print("Palindrome")
+else:
+  print("not palindrome")
+
+
+a="LEMON MELON"
+b=a[::-1]
+if a==b:
+  print("Palindrome")
+else:
+  print("not palindrome")
