25699c6ee82c674ba0b6c4611034df850d34cf3b
sravanthi-99 <parisesravanthi0@gmail.com>
2025-03-18 07:42:14 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,18 @@
+#calculator logic
+a=int(input())
+op=input()
+b=int(input())
+if op=="+":
+    print(a+b)
+elif op=="-":
+    print(a-b)
+elif op=="*":
+    print(a*b)
+elif op=="/":
+    print(a/b)
+elif op=="%":
+    print(a%b)
+elif op=="//":
+    print(a//b)
+elif op=="**":
+    print(a**b)
