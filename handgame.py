9d13c6dc1a302f0a4bc41c245cb8ff129abcae82
sravanthi-99 <parisesravanthi0@gmail.com>
2025-03-18 07:52:37 +0530

    Create handgame.py

---
+++ handgame.py
@@ -0,0 +1,17 @@
+#Hand game:rock,paper,scissor
+a=input()
+b=input()
+if a=="rock" and b=="paper":
+   print("b is winner")
+elif a=="rock" and b=="scissor":
+   print("a is winner")
+elif a=="paper" and b=="rock":
+   print("b is winner")
+elif a=="paper" and b=="scissor":
+   print("b is winner")
+elif a=="scissor" and b=="rock":
+   print("b is winner")
+elif a=="scissor" and b=="paper":
+   print("a is winner")
+else:
+   print("Tie")
